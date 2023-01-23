d1f7c47bb3d4d6342be05a32eb29093fae8d87c8
manu5525 <108022894+manu5525@users.noreply.github.com>
2023-01-23 17:47:17 +0530

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -43,10 +43,7 @@ module.exports = {
     "arrow-spacing": [
       "error", { "before": true, "after": true }
     ],
-    "no-console": "off",
+    "no-console": "error",
     "react/prop-types": 0
-  },
-  "globals": {
-    "process": true
   }
-}
+  }
