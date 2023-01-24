368aa4eab46dbcbe2c468a6a53a3a3338de6f530
manu5525 <108022894+manu5525@users.noreply.github.com>
2023-01-24 15:28:53 +0530

    Create pokedex.cy.js

---
+++ cypress/e2e/pokedex.cy.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+describe('Pokedex', function() {
+  it('front page can be opened', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+
+  it('Pokemon card can be opened', function(){
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+    cy.contains('overgrow')
+  })
+
+})
